48786bc947f8e86b280d795e9bfd8899df7c0a59
Emanuel Lauria <emanuel.lauria@gmail.com>
2013-11-25 20:50:11 +0100

    removed unused async library from bookmarklet

--- discogs.bookmark.js
+++ discogs.bookmark.js
@@ -1,12 +1,8 @@
 javascript:(function() {
     var url = "http://elauria.github.io/dgexplorer/index.html?ids=";
     var script_tag = document.createElement('script');
-    var async_tag = document.createElement('script');
     script_tag.setAttribute("type", "text/javascript");
-    async_tag.setAttribute("type", "text/javascript");
-    async_tag.setAttribute("src", "//raw.github.com/caolan/async/master/lib/async.js");
     script_tag.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js");
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(async_tag);
     (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
 
     script_tag.onload = function() {    window.jQuery.noConflict();
